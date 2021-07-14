237c4f1bd3cdbd5c3ffee45408ede0b91f7271e8
ts <shanksleslietrey@gmail.com>
2021-07-14 09:34:09 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction()
+{
+    document.getElementById('fish__name').innerHTML = "Lipstick Tang"
+}
---
+++ scripts/other.js
@@ -0,0 +1,42 @@
+function showPic() 
+{
+    var sourceOfPicture = "images/fish1.jpeg";
+    var img = document.getElementById('fish1')
+    img.src = sourceOfPicture.replace();
+    img.style.display = "block";
+}
+
+function showPic2() 
+{
+    var sourceOfPicture = "images/fish2.jpeg";
+    var img = document.getElementById('fish2')
+    img.src = sourceOfPicture.replace();
+    img.style.display = "block";
+}
+
+function showPic3() 
+{
+    var sourceOfPicture = "images/fish3.jpeg";
+    var img = document.getElementById('fish3')
+    img.src = sourceOfPicture.replace('90x90', '225x225');
+    img.style.display = "block";
+}
+
+function showPic4() 
+{
+    var sourceOfPicture = "images/fish4.jpeg";
+    var img = document.getElementById('fish4')
+    img.src = sourceOfPicture.replace('90x90', '225x225');
+    img.style.display = "block";
+}
+
+function showPic5() 
+{
+    var sourceOfPicture = "images/fish5.jpeg";
+    var img = document.getElementById('fish5')
+    img.src = sourceOfPicture.replace('90x90', '225x225');
+    img.style.display = "block";
+}
+
+
+
